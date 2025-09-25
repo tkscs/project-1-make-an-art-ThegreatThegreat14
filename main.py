6dc8239edde447d78f88fd00e4423fb344042db3
ThegreatThegreat14 <amaydan2028@kehillahstudent.org>
2025-09-25 09:00:31 -0700

    hiiiiii

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import turtle
 
-def big_shape(num_sides):
+def shape(size, num_sides):
     for i in range(num_sides):
-       turtle.forward(1000/num_sides)
-       turtle.right(360/num_sides)
-
-def small_shape(num_sides):
-    for i in range(num_sides):
-       turtle.forward(280/num_sides)
+       turtle.forward(size/num_sides)
        turtle.right(360/num_sides)
 
 turtle.left (90)
@@ -15,7 +10,7 @@ turtle.up()
 turtle.forward (100)
 turtle.down()
 turtle.right (90)
-big_shape (50)
+shape (1000,50)
 
 turtle.up()
 turtle.right (90)
@@ -25,13 +20,13 @@ turtle.right (90)
 turtle.forward (30)
 turtle.down()
 turtle.left (90)
-small_shape (100)
+shape (280,100)
 turtle.left (90)
 turtle.up()
 turtle.forward (60)
 turtle.left (90)
 turtle.down()
-small_shape (100)
+shape (280,100)
     
 
 turtle.exitonclick()
