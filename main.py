693dc5e4dfcd568ee0b74de2848b4dbc66ef0336
ThegreatThegreat14 <amaydan2028@kehillahstudent.org>
2025-10-16 13:17:40 -0700

    shiiinnnnyyyyyyyy

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ import turtle
 import numpy as np
 turtle.tracer (0,0)
 
+shinysize = 1
+
 def shape(size, num_sides):
     for i in range(num_sides):
        turtle.forward(size/num_sides)
        turtle.right(360/num_sides)
+
+def part_shape(size, num_sides):
+    turtle.forward(size)
+    turtle.right(360/num_sides)
 
 # head
 turtle.left (90)
@@ -21,18 +27,94 @@ turtle.up()
 turtle.right (90)
 turtle.forward (120)
 
-# eyes
+# left eye
 turtle.right (90)
 turtle.forward (30)
 turtle.down()
 turtle.left (90)
 shape (280,100)
+
+# left shinies
+turtle.up()
+turtle.right (180)
+for i in range(9):
+    part_shape (2.8,-100)
+turtle.left (90)
+turtle.forward (10)
+turtle.down()
+turtle.right (90)
+for i in range(100):
+    part_shape (shinysize,-100)
+for i in range(50):
+    part_shape (shinysize,-100)
+turtle.up()
+turtle.right (90)
+turtle.forward (10)
+turtle.down()
+turtle.right (90)
+for i in range(100):
+    part_shape (2*shinysize/3,-100)
+turtle.up()
+turtle.right (90)
+turtle.forward (10)
+turtle.down()
+turtle.right (90)
+for i in range(50):
+    part_shape (shinysize,-100)
+turtle.up()
+turtle.right (90)
+turtle.forward (10)
+turtle.right (90)
+for i in range(9):
+    part_shape (2.8,100)
+turtle.down()
+
+# right eye
 turtle.left (90)
 turtle.up()
 turtle.forward (60)
 turtle.left (90)
 turtle.down()
 shape (280,100)
+
+# right shinies
+for i in range (50):
+    part_shape (2.8,100)
+turtle.up()
+turtle.right (180)
+for i in range(9):
+    part_shape (2.8,-100)
+turtle.left (90)
+turtle.forward (10)
+turtle.down()
+turtle.right (90)
+for i in range(100):
+    part_shape (shinysize,-100)
+for i in range(50):
+    part_shape (shinysize,-100)
+turtle.up()
+turtle.right (90)
+turtle.forward (10)
+turtle.down()
+turtle.right (90)
+for i in range(100):
+    part_shape (2*shinysize/3,-100)
+turtle.up()
+turtle.right (90)
+turtle.forward (10)
+turtle.down()
+turtle.right (90)
+for i in range(50):
+    part_shape (shinysize,-100)
+turtle.up()
+turtle.right (90)
+turtle.forward (10)
+turtle.right (90)
+for i in range(9):
+    part_shape (2.8,100)
+for i in range (50):
+    part_shape (2.8,100)
+turtle.down()
 
 # nose
 turtle.left (90)
@@ -42,20 +124,17 @@ turtle.left (90)
 turtle.forward (35)
 turtle.down()
 turtle.left (90)
-def part_shape(num_sides):
-    turtle.forward(1)
-    turtle.right(360/num_sides)
 for i in range(200, 280, 2):
-    part_shape(i)
+    part_shape(1,i)
 turtle.right (29)
 for i in range(180, 450, 5):
-    part_shape(i)
+    part_shape(1,i)
 turtle.right (45)
 for i in range(450, 180, -5):
-    part_shape(i)
+    part_shape(1,i)
 turtle.right (29)
 for i in range(200, 280, 2):
-    part_shape(i)
+    part_shape(1,i)
 turtle.forward (1)
 turtle.right (360/282)
 turtle.forward (1)
