eb905bbdadb08956675c8ed6961e7caedc0637c3
ThegreatThegreat14 <amaydan2028@kehillahstudent.org>
2025-10-16 08:37:30 -0700

    sniff sniff sniff

--- main.py
+++ main.py
@@ -7,12 +7,6 @@ def shape(size, num_sides):
        turtle.forward(size/num_sides)
        turtle.right(360/num_sides)
 
-def semi(size, num_sides):
-    for i in range(num_sides):
-       turtle.forward(size/num_sides)
-       turtle.right(90/num_sides)
-
-
 # head
 turtle.left (90)
 turtle.up()
@@ -56,18 +50,24 @@ for i in range(200, 280, 2):
 turtle.right (29)
 for i in range(180, 450, 5):
     part_shape(i)
-turtle.right (44)
+turtle.right (45)
 for i in range(450, 180, -5):
     part_shape(i)
 turtle.right (29)
 for i in range(200, 280, 2):
     part_shape(i)
+turtle.forward (1)
+turtle.right (360/282)
+turtle.forward (1)
+turtle.right (360/284)
 
+# mouth
 turtle.up()
-turtle.forward (35)
+turtle.right (90)
+turtle.forward (60)
+turtle.down()
 
 
     
 turtle.update()
-
 turtle.exitonclick()
